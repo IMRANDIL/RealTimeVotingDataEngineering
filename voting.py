599d0817aa94dd1cbe5582c81b4c676d77e9cb7a
IMRANDIL <aliimranadil2@gmail.com>
2024-04-20 17:25:20 +0530

    added modification on voting candidate fetch

--- voting.py
+++ voting.py
@@ -10,6 +10,24 @@ if __name__ == "__main__":
         try:
             cursor = conn.cursor()
             
+            candidates_query = cursor.execute("""
+                                              
+                                             SELECT json_build_object(
+           'candidate_id', candidate_id,
+           'candidate_name', candidate_name,
+           'party_affiliation', party_affiliation,
+           'biography', biography,
+           'campaign_platform', campaign_platform,
+           'photo_url', photo_url
+       ) AS candidate_json
+FROM candidates;
+""")
+            candidates = [candidate[0] for candidate in cursor.fetchall()]
+            print(candidates)
+            
         except Exception as e:
             # Print a generic error message for any exceptions
             print(f"An error occurred: {e}")
+            
+        finally:
+            conn.close()
